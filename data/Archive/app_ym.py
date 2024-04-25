ce050b460a04f5a02c5deca2cd7043e7f275ce49
Yuan Man <158439434+yuanman88@users.noreply.github.com>
2024-04-25 15:35:14 +0800

    Add files via upload

---
+++ data/Archive/app_ym.py
@@ -0,0 +1,70 @@
+import streamlit as st
+from llama_index import VectorStoreIndex, ServiceContext, Document, StorageContext, load_index_from_storage
+from llama_index.llms import OpenAI
+import openai
+from PIL import Image
+import random
+from dotenv import load_dotenv, find_dotenv
+import os
+
+st.set_page_config(page_title="Chat with Your Feng Shui AI Master", page_icon="👩🏻‍🏫", layout="centered", initial_sidebar_state="auto", menu_items=None)
+
+_ = load_dotenv(find_dotenv()) # read local .env file
+openai.api_key = os.environ['OPENAI_API_KEY']
+
+image = Image.open('st_banner.png')
+st.image(image, caption='Welcome to the AI Feng Shui Master!',width=1000)
+
+@st.cache_resource(show_spinner=False)
+def load_data():
+    with st.spinner(text="Doing some calculation – hang tight! This should take 1-2 minutes."):
+        
+        # Rebuild the storage context
+        storage_context = StorageContext.from_defaults(persist_dir="./data/index.vecstore")
+
+        # Load the index
+        index = load_index_from_storage(storage_context)
+
+        # Load the finetuned model 
+        ft_model_name = "ft:gpt-3.5-turbo-1106:personal::9CTyKWAT"
+        ft_context = ServiceContext.from_defaults(llm=OpenAI(model=ft_model_name, temperature=0.3), 
+        context_window=2048, 
+        
+        system_prompt="""
+       You are an AI trained to assist with living space Feng Shui related question.
+       Give the correct recommendation.
+        """
+        )           
+        return index
+
+index = load_data()
+chat_engine = index.as_chat_engine(chat_mode="openai", verbose=True)
+
+if "messages" not in st.session_state.keys(): # Initialize the chat messages history
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Ask me Feng Shui question 😊"}
+    ]
+
+if prompt := st.chat_input("Ask Me Any Feng Shui Question On Living Space"):
+    # Save the original user question to the chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.session_state.new_question = True
+
+    # Create a detailed prompt for the chat engine
+    chat_history = ' '.join([message["content"] for message in st.session_state.messages])
+    detailed_prompt = f"{chat_history} {prompt}"
+
+if "new_question" in st.session_state.keys() and st.session_state.new_question:
+   for message in st.session_state.messages: # Display the prior chat messages
+       with st.chat_message(message["role"]):
+           st.write(message["content"])
+   st.session_state.new_question = False # Reset new_question to False
+
+# If last message is not from assistant, generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+   with st.chat_message("assistant"):
+       with st.spinner("Calculating..."):
+           response = chat_engine.chat(detailed_prompt)
+           st.write(response.response)
+           # Append the assistant's detailed response to the chat history
+           st.session_state.messages.append({"role": "assistant", "content": response.response})
